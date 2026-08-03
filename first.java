86d94fa8763c6fb921c18960f0ee2ae4f375c2c8
Pushpendra Singh <pushpendra.25scs1003002616@iilm.edu>
2026-08-03 14:37:30 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,7 @@
+public class first{
+    public static void main(String[] ars){
+        System.out.println("Name = Pushpendra Singh");
+        System.out.println("Roll.No. = 25scs1003002616");
+        System.out.println("Subject = Java Programming");
+    }
+}
