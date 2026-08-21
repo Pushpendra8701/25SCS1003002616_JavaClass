4bf2e0d28da80f895c146d9b851dd0d503148ab1
Pushpendra Singh <pushpendra.25scs1003002616@iilm.edu>
2026-08-22 03:00:54 +0530

    Add files via upload

---
+++ Program12.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Program12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+        sc.close();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                count++;
+            }
+        }
+        System.out.println("Count = " + count);
+    }
+}
