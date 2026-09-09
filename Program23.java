fe49e3f660b311b5481de2ecc397f6ed0b092910
Pushpendra Singh <pushpendra.25scs1003002616@iilm.edu>
2026-09-09 23:40:52 +0530

    Add files via upload

---
+++ Program23.java
@@ -0,0 +1,35 @@
+class Display {
+
+    void show(String name) {
+        System.out.println("Name: " + name);
+    }
+
+    void show(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    void show(String name, int age, double marks) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
+
+public class Program23 {
+
+    public static void main(String[] args) {
+
+        Display d = new Display();
+
+        d.show("Pushpendra");
+
+        System.out.println();
+
+        d.show("Pushpendra", 20);
+
+        System.out.println();
+
+        d.show("Pushpendra", 20, 85.5);
+    }
+}
---
+++ Program24.java
@@ -0,0 +1,23 @@
+class Maximum {
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+    int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+    double max(double a, double b) {
+        return (a > b) ? a : b;
+    }
+}
+
+public class Program24 {
+
+    public static void main(String[] args) {
+        Maximum m = new Maximum();
+        System.out.println("Maximum of two integers: " + m.max(10, 20));
+
+        System.out.println("Maximum of three integers: " + m.max(10, 25, 15));
+
+        System.out.println("Maximum of two decimal numbers: " + m.max(10.5, 20.8));
+    }
+}
